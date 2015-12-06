45bec05499f513f59f6c12020bc406f90f9d3f2b
dreamer2018 <zhoupans_mail@163.com>
2015-12-06 17:51:41 +0800

    add

--- sh04.sh
+++ sh04.sh
@@ -3,11 +3,13 @@
 # Author: ZhouPan / github:dreamer2018 
 # Mail: zhoupans_mail@163.com
 # Blog: blog.csdn.net/it_dream_er
-# Function:
+# Function:输入两数，然后求和
 # Created Time: Sun 06 Dec 2015 05:04:41 PM CST
 
 echo -e "Please input TWO number:"
 read -p "first number:" fnum
 read -p "second number:" snum
-declare -i total=$fnum*$snum
+#declare -i total=$fnum*$snum
+total=$(($fnum*$snum))
 echo -e "The result is $total\n"
+
