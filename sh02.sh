89dd403e35b9758233361539ff9bc12184f757ef
dreamer2018 <zhoupans_mail@163.com>
2015-12-06 16:45:05 +0800

    add

---
+++ sh02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File Name: sh02.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:用户输入firstname和lastname，输出用户的名字
+# Created Time: Sun 06 Dec 2015 04:38:07 PM CST
+read -p "Please Input Your Firstname:" Firstname
+read -p "Please Input Your Lastname:" Lastname
+echo -e "Your Name is: $Firstname $Lastname\n"
