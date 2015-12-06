9f2ae091bb1408c8ce146e5488cfb4a8e65304ec
dreamer2018 <zhoupans_mail@163.com>
2015-12-06 16:26:22 +0800

    add

---
+++ sh01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# File Name: sh01.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:我的第一个shell script练习
+# Created Time: Sun 06 Dec 2015 04:25:04 PM CST
+echo -e "hello world ! \n"
+exit 0
