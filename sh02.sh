261338a7dfa5051f5e2b1c632f094bdbe198e7b7
dreamer2018 <zhoupans_mail@163.com>
2015-12-06 16:46:50 +0800

    debug

--- sh02.sh
+++ sh02.sh
@@ -5,6 +5,7 @@
 # Blog: blog.csdn.net/it_dream_er
 # Function:用户输入firstname和lastname，输出用户的名字
 # Created Time: Sun 06 Dec 2015 04:38:07 PM CST
+
 read -p "Please Input Your Firstname:" Firstname
 read -p "Please Input Your Lastname:" Lastname
 echo -e "Your Name is: $Firstname $Lastname\n"
