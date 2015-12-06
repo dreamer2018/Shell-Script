df8ebe0c71ce4c8b60962075473697b203f41964
dreamer2018 <zhoupans_mail@163.com>
2015-12-06 17:09:39 +0800

    add

---
+++ sh04.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# File Name: sh04.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: Sun 06 Dec 2015 05:04:41 PM CST
+
+echo -e "Please input TWO number:"
+read -p "first number:" fnum
+read -p "second number:" snum
+declare -i total=$fnum*$snum
+echo -e "The result is $total\n"
