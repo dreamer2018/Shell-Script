adc88a63b659682bb2fdc5023895f7a054f14502
dreamer2018 <zhoupans_mail@163.com>
2015-12-14 15:46:37 +0800

    add

---
+++ sh14.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# File Name: sh14.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: Mon 14 Dec 2015 03:31:16 PM CST
+s=0
+i=0
+while [ "$s" != "100" ]
+do
+    i=$( ( $i + 1 ) )
+    s=$( ( $s + $i ) )
+done
+echo "1+2+3+...+100 = $s"
