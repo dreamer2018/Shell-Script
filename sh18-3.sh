067243869d715e35b2d318a7a8d3604f465fa117
dreamer2018 <zhoupans_mail@163.com>
2015-12-14 18:40:59 +0800

    add

---
+++ sh18-3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# File Name: sh18-3.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: Mon 14 Dec 2015 06:38:12 PM CST
+for i in $(seq 1 100)
+do
+    echo $i
+done
