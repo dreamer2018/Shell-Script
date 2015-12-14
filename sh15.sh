46962cca6f164b2aac7e01dbc5e89f41ad80abfe
dreamer2018 <zhoupans_mail@163.com>
2015-12-14 16:32:02 +0800

    add

---
+++ sh15.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# File Name: sh15.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: Mon 14 Dec 2015 04:13:58 PM CST
+for animals in dog cat elephant
+do
+    echo "There is ${animals}"
+done
