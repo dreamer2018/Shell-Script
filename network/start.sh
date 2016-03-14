3ef617800f7eab98743cd65baca2e3d10aa0b988
dreamer2018 <zhoupans_mail@163.com>
2016-03-14 18:53:35 +0800

    start.sh

---
+++ network/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File Name: start.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: 2016年03月14日 星期一 18时51分52秒
+nohup ./net_start.sh &
