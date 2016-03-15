c05bb7c22ea36656a6d7a5faf6103f2c16eb589d
dreamer2018 <zhoupans_mail@163.com>
2016-03-15 13:20:21 +0800

    加入了进程数量监控，进程过多就将一部分杀掉

--- network/net_start.sh
+++ network/net_start.sh
@@ -11,9 +11,20 @@ do
     #检测192.168.20.1服务器上的./connect.sh进程是否还活着，活着则返回>1
     exist=$( ssh root@192.168.20.1 ps aux | grep './conn' | wc -l )
     #检测网络是否通着
-    ping -W 1 -c 1 106.38.241.48 > /dev/null
+    ping -W 2 -c 1 180.97.33.108 > /dev/null
     #如果网络是通着的话，network值就为0
     network=$?
+    #允许同时运行一到三个conn.sh进程，如果大于三个则将其余的杀掉
+    if [ $exist -gt 3 ];then
+        del=$exist -3
+        for i in $( seq 1 $del )
+        do
+            #获取进程的PID
+            pid=ssh root@192.168.20.1 ps aux | grep './conn' | awk '{ print $2 }' | sed -n "1p"
+            #将进程杀死
+            ssh root@192.168.20.1 kill -9 $pid
+        done
+    fi
     #如果网络不通或者conn.sh进程死掉了，就重新启动
     if [ $exist -lt 1 ] || [ $network -gt 0 ];then
         #重新启动conn.sh脚本
