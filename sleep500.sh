dd45766946b23a6edcf8a2d98de07e6e4f7d8db1
dreamer2018 <zhoupans_mail@163.com>
2016-03-25 16:47:46 +0800

    add

---
+++ sleep500.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# File Name: sleep500.sh
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: 2015年12月20日 星期日 22时28分55秒
+/bin/sleep 500s
+/bin/echo "I have slept 500 seconds"
